44836d775deec12d2c9363ff397d25ef096fda99
Didik Hadumi Setiaji <dhs0223@gmail.com>
2022-06-10 16:13:53 +0700

    First commit

---
+++ src/EmailValidator.php
@@ -0,0 +1,32 @@
+<?php
+namespace DXH30\EmailValidator;
+require './vendor/autoload.php';
+
+class Validate {
+    private $blacklistedDomains;
+
+    /* @description
+     * validate email if it is not inside blacklisted domains
+     * @return boolean
+     */
+    public function validate($email) {
+        if (in_array($this->blacklistedDomains, $domain)) { 
+            throw new Exception("the email domain $domain is inside blacklisted domains");
+        }
+    }
+
+    /* @description
+     * set blacklisted domains
+     */
+    public function setBlacklistedDomains($domains = array()) {
+        $this->blacklistedDomains = $domains;
+    }
+
+    /* @description
+     * get blacklisted domains
+     */
+
+    public function getBlacklistedDomains() {
+        return $this->blacklistedDomains;
+    }
+}
